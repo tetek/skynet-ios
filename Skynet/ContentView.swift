a7ac12e1342e6f04dfca85d407f29c88f238a56c
Wojtek Mandrysz <tetek1@gmail.com>
2020-02-29 22:25:53 +0100

    Initial Commit

---
+++ Skynet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Skynet
+//
+//  Created by Wojciech Mandrysz on 29/02/2020.
+//  Copyright © 2020 Wojciech Mandrysz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
